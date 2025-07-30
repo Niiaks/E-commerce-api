ab1290c2bc15c994d4015e22d035986b62b74e2e
Niiaks <napappoe007@st.ug.edu.gh>
2025-07-30 11:44:13 +0000

    added rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,9 +16,26 @@ import { RedisService } from './redis/redis.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { entities } from './config/typeorm.config';
-
+import { ThrottlerModule } from '@nestjs/throttler';
 @Module({
   imports: [
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 20,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
